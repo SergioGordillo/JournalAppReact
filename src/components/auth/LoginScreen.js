15455c8bf52b245c2aceff3a4bfcdf6322ec635d
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-12-07 00:06:35 +0100

    feat: login screen styles implemented

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,28 +1,48 @@
 import React from 'react'
 
+import { Link } from "react-router-dom";
+
 export const LoginScreen = () => {
     return (
         <>
-            <h3>Login</h3>
+            <h3 className="auth__title">Login</h3>
 
             <form>
                 <input type="text"
-                       placeholder="Write your email here"
-                       name="email"
+                    placeholder="Email"
+                    name="email"
+                    className="auth__input"
+                    autoComplete="off"
                 />
                 <input type="password"
-                       placeholder="Write your password here"
-                       name="email"
+                    placeholder="Password"
+                    name="email"
+                    className="auth__input"
                 />
 
                 <button type="submit"
+                        className="btn btn-primary btn-block"
                 >
                     Login
                 </button>
 
-                <hr/>
+                <div className="auth__social-networks">
+                    <p>Login with Social Networks</p>
+                    <div
+                        className="google-btn"
+                    >
+                        <div className="google-icon-wrapper">
+                            <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+                        </div>
+                        <p className="btn-text">
+                            <b>Sign in with Google</b>
+                        </p>
+                    </div>
+                </div>
 
-                Google
+                <Link to="/auth/register">
+                    Create new account
+                </Link>
 
             </form>
         </>
